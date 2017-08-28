14d71438e70365edec4f1fe548b4bf7ed3740d73
Manjunatha Nagaraj <i074029@sap.com>
2017-08-28 16:54:58 +0530

    Initial Commit

---
+++ githubexperiments/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  githubexperiments
+//
+//  Created by Nagaraj, Manjunatha on 28/08/17.
+//  Copyright © 2017 SAP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
